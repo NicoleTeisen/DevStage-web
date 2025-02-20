83e20e653b29fa5ece84f551ee4802ec96572329
nicoleteisen <nicole.camargo@gmail.com>
2025-02-20 12:08:18 -0300

    feat: create Input component

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from "react";
+import { ReactNode, ComponentProps } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-}
+interface ButtonProps extends ComponentProps<"button"> {}
 
 export function IconButton(props: ButtonProps) {
   return (
-    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-      {props.children}
-    </button>
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
   );
 }
