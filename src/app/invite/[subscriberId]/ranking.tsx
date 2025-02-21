9a7ebedca565f41696a68703277889d1c5f574fd
nicoleteisen <nicole.camargo@gmail.com>
2025-02-21 14:10:28 -0300

    refactor: update application to integrate with API

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,9 +1,12 @@
+import { getRanking } from "@/http/api";
 import Image from "next/image";
 import cooper from "../../../assets/medal-cooper.svg";
 import gold from "../../../assets/medal-gold.svg";
 import silver from "../../../assets/medal-silver.svg";
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking();
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -11,39 +14,39 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Nicole Teisen
-          </span>
+        {ranking.map((rank, index) => {
+          const rankingPosition = index + 1;
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <div
+              key={rank.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{" "}
+                {rank.name}
+              </span>
 
-          <Image src={gold} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Vitor Chuo
-          </span>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {rank.score}
+              </span>
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            750
-          </span>
+              {rankingPosition === 1 && (
+                <Image src={gold} alt="" className="absolute top-0 right-8" />
+              )}
+              {rankingPosition === 2 && (
+                <Image src={silver} alt="" className="absolute top-0 right-8" />
+              )}
+              {rankingPosition === 3 && (
+                <Image src={cooper} alt="" className="absolute top-0 right-8" />
+              )}
+            </div>
+          );
+        })}
 
-          <Image src={silver} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | João Fernandes
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            315
-          </span>
-
-          <Image src={cooper} alt="" className="absolute top-0 right-8" />
-        </div>
+        {ranking.length === 0 && (
+          <p className="text-gray-300">Nenhuma indicação registrada</p>
+        )}
       </div>
     </div>
   );
